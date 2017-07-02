885a8509c2fc0f005c5a50e414a8980e2c831adc
Marko Kevac <marko@kevac.org>
2017-07-02 18:32:59 +0300

    Initial commit

---
+++ hmm.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"os"
+
+	"log"
+	"net/http"
+
+	"bufio"
+
+	"fmt"
+	"net"
+
+	"github.com/gorilla/websocket"
+	"github.com/jessevdk/go-flags"
+	"github.com/mkevac/hmm/internal/browser"
+)
+
+var opts struct {
+	File string `short:"f" long:"file" description:"Use this file instead of stdin"`
+	Http string `long:"http" description:"Address on which to listen to" default:"localhost:0"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request, input chan string) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for line := range input {
+		if err := conn.WriteJSON(line); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func main() {
+	flags.Parse(&opts)
+
+	var r *bufio.Reader
+
+	if opts.File != "" {
+		f, err := os.Open(opts.File)
+		if err != nil {
+			log.Fatalf("Error while opening '%s': %s", opts.File, err)
+		}
+		defer f.Close()
+		r = bufio.NewReader(f)
+	} else {
+		r = bufio.NewReader(os.Stdin)
+	}
+
+	output := make(chan string, 1024)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := indexHtmlTemplate.Execute(w, nil); err != nil {
+			log.Print("Error while executing index.html template")
+		}
+	})
+	http.HandleFunc("/main.js", func(w http.ResponseWriter, r *http.Request) {
+		if err := mainJsTemplate.Execute(w, nil); err != nil {
+			log.Print("Error while executing main.js template")
+		}
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r, output)
+	})
+
+	ln, err := net.Listen("tcp", opts.Http)
+	if err != nil {
+		log.Fatalf("Error while listening on '%s': %s", opts.Http, err)
+	}
+
+	log.Printf("Opening browser")
+
+	if !browser.Open("http://" + ln.Addr().String()) {
+		fmt.Fprintf(os.Stderr, "Hmm is listening on http://%s\n", ln.Addr().String())
+	}
+
+	go func() {
+		if err := http.Serve(ln, nil); err != nil {
+			log.Fatal("Error while serving http: ", err)
+		}
+	}()
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		text := s.Text()
+		fmt.Println(text)
+		output <- text
+	}
+	if err := s.Err(); err != nil {
+		log.Fatalf("Error while scanning file: %s", err)
+	}
+}
---
+++ templates.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"log"
+	"text/template"
+)
+
+var indexHtmlTemplate *template.Template
+var mainJsTemplate *template.Template
+
+var indexHtmlTemplateString = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+    <meta charset="utf-8" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
+<body>
+<div id="container1"></div>
+<script src="main.js"></script>
+</body>
+</html>
+
+`
+
+var mainJsTemplateString = `
+var chart1;
+var headers = [];
+
+$(function () {
+
+    function wsurl() {
+        var l = window.location;
+        return ((l.protocol === "https:") ? "wss://" : "ws://") + l.hostname + (((l.port != 80) && (l.port != 443)) ? ":" + l.port : "") + "/ws";
+    }
+
+    ws = new WebSocket(wsurl());
+    ws.onopen = function () {
+        ws.onmessage = function (evt) {
+
+            var data = JSON.parse(evt.data);
+            console.log(data);
+            var splitted = data.split('\t');
+
+            if (headers.length == 0) {
+                headers = splitted;
+
+                var chartData = [];
+                var chartLayout = {
+                    autosize: true,
+                    yaxis: {
+                        tickformat: ".5s"
+                    }
+                };
+
+                for (i = 1; i < headers.length; i++) {
+                    chartData.push({x: [], y: [], type: "scatter", name: headers[i]})
+                }
+
+                chart1 = Plotly.newPlot('container1', chartData, chartLayout);
+
+                return;
+            }
+
+            //var d = moment(splitted[0]).format('HH:mm:ss');
+
+            var xses = [];
+            var yses = [];
+            var numbers = [];
+
+            for (i = 1; i < splitted.length; i++) {
+                xses.push([splitted[0]]);
+                yses.push([parseInt(splitted[i])]);
+                numbers.push(i - 1);
+            }
+
+            Plotly.extendTraces('container1', {x: xses, y: yses}, numbers, 86400);
+        }
+    };
+});
+`
+
+func init() {
+	var err error
+
+	indexHtmlTemplate, err = template.New("").Parse(indexHtmlTemplateString)
+	if err != nil {
+		log.Fatal("Error while parsing index.html template")
+	}
+
+	mainJsTemplate, err = template.New("").Parse(mainJsTemplateString)
+	if err != nil {
+		log.Fatal("Error while parsing main.js template")
+	}
+}
